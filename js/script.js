078af68fbb4327dbbce4c457c8eb48e164cc3c49
heilikpd <heilikpd@gmail.com>
2019-07-25 16:55:04 +0300

    change class name

--- js/script.js
+++ js/script.js
@@ -1,37 +1,43 @@
-import addSidebar from './sidebar.js'
-import css from '../css/index.css'
-import App from './App'
-import getData from './serviсe'
+import addSidebar from "./sidebar.js";
+import css from "../css/index.css";
+import App from "./App";
+import getData from "./serviсe";
 
+let cardModule = Array.from(document.getElementsByClassName("card"));
+console.log(cardModule);
+cardModule.forEach(item => (item.onclick = addSidebar));
 
-let cardModule = Array.from(document.getElementsByClassName("card_link"))
-console.log(cardModule)
-cardModule.forEach (item => item.onclick = addSidebar)
+const container = new App();
+container.render();
 
-
-const container = new App()
-container.render()
-
-  
 getData().then(response => {
-   // keys / displayName 
-   let statuses = Array.from (new Set(response.reduce((acc, curr) =>
-         [...acc, ...curr.modules.map(item => item.moduleStatus.displayName)], [])))
-   console.log(statuses)
-   let result = response.reduce((acc, curr) => {
-         return [...acc, ...curr.modules.filter(item => item.moduleStatus.displayName === `${statuses[0]}`)]
-         }, [])
+  // keys / displayName
+  let statuses = Array.from(
+    new Set(
+      response.reduce(
+        (acc, curr) => [
+          ...acc,
+          ...curr.modules.map(item => item.moduleStatus.displayName)
+        ],
+        []
+      )
+    )
+  );
+  console.log(statuses);
+  let result = response.reduce((acc, curr) => {
+    return [
+      ...acc,
+      ...curr.modules.filter(
+        item => item.moduleStatus.displayName === `${statuses[0]}`
+      )
+    ];
+  }, []);
 
-   result.forEach (item => {
-      let title = document.createElement ("div")
-      title.textContent = item.moduleTitle
-      title.appendChild (document.createElement ("div"))
-          .textContent = "Lorem"
-      title.style.border = "1px solid gray"
-      document.body.appendChild (title)
-   })
-
-})
-   
- 
- 
+  result.forEach(item => {
+    let title = document.createElement("div");
+    title.textContent = item.moduleTitle;
+    title.appendChild(document.createElement("div")).textContent = "Lorem";
+    title.style.border = "1px solid gray";
+    document.body.appendChild(title);
+  });
+});
