cbafc161b71608266733b595e4d18596389b5fea
heilikpd <heilikpd@gmail.com>
2019-07-28 14:59:10 +0300

    add first version of dynamic scroll

--- js/draft.js
+++ js/draft.js
@@ -1,14 +1,46 @@
 import getData from "./serviсe";
 export class App {
+  constructor() {
+    this.stageContainer = new Stage();
+  }
   init() {
     const data = getData();
     let container = document.getElementsByClassName("gen-wrapper")[0];
     container.innerHTML = `${app}`;
 
-    new Dashboard().render(data)
+    new Dashboard().render(data);
+    this.stageContainer.elem.addEventListener(
+      "scroll",
+      e => this.stageActiveScroll(e),
+      true
+    );
+  }
+  stageActiveScroll(e) {
+    const target = e.target;
+    if (!target.classList.contains("cards_container")) {
+      return;
+    }
+    if (target.scrollHeight - target.scrollTop === e.target.offsetHeight) {
+      const stageTitle = target.dataset.title;
+      const [stageToAppent] = this.stageContainer.stages.filter(
+        stage => stage.title === stageTitle
+      );
+      const loadedCourses = this.data.retreiveCourses(stageTitle, 10);
+      stageToAppent.append(loadedCourses);
+    }
+  }
+  retreiveCourses(stageName, amount) {
+    const [requiredStage] = this.cloneData.filter(
+      stage => stageName === stage.name
+    );
+    return requiredStage.courses.splice(0, amount);
   }
 }
 
+export class Sidebar {
+  constructor() {}
+  render() {}
+}
 export class Dashboard {
   constructor() {}
   render(allData) {
